bf57d15a46c646313c14025a7fc0cec570acb423
zyd <zhaoyanda@163.com>
2019-03-08 10:56:28 +0800

    as

--- resources/views/weixin/submit.blade.php
+++ resources/views/weixin/submit.blade.php
@@ -1,87 +1,103 @@
 @extends('layout.goods')
 
+@section('title') {{$title}}    @endsection
+
+@section('nav')
+    @parent
+@endsection
+
 @section('content')
-    <div class="type_menu" style="margin-bottom: 20px;">
+    <div class="top_menu" style="margin-bottom: 20px;">
         <p>
-            <button id="btn1">一级按钮</button>
-            名字:<input type="text" name="top_name">
-            <button class="top_a">克隆</button>
-        </p>
-        <div style="margin-bottom: 20px; bottom:5px; class:'top_type' ">
-            <button class="top_n">二级按钮</button>
-            <button class="top_s">克隆</button>
-        </div>
-        <div>
-            按钮类型：<select name="top_menu">
-                <option value="php">php</option>
-                <option value="view">view</option>
-            </select>
-            二级按钮名字：<input type="text" name="two_btn_name"><br>
-            二级按钮url：<input type="text" name="two_btn_url"><br>
-            二级按钮名字key：<input type="text" name="two_btn_key">
+            <button>一级菜单按钮</button>
+            名称：<input type="text" class="top_name">
+            <button class="top_btn">克隆</button>
+        </p >
+        <div style="margin-left: 20px;margin-bottom: 5px;" class="two_menu">
+            <button>二级菜单按钮</button>&nbsp;&nbsp;<button class="two_btn">克隆</button>
+            <div>
+                按钮类型：<select class="two_btn_type">
+                    <option value="view">VIEW</option>
+                    <option value="click">CLICK</option>
+                </select></br>
+                二级按钮名称：<input type="text" class="two_btn_name"></br>
+                二级按钮url：<input type="text" class="two_btn_url"></br>
+                二级按钮key：<input type="text" class="two_btn_key">
+            </div>
         </div>
     </div>
-@endsection
+    <div>
+        <button id="sub_btn">发布</button>
+    </div>
+@endsection'
+
 @section('footer')
     @parent
-    <script src="https://res2.wx.qq.com/open/js/jweixin-1.4.0.js"></script>
-
     <script>
-        wx.config({
-            debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-            appId: "{{$jsconfig['appid']}}", // 必填，公众号的唯一标识
-            timestamp: {{$jsconfig['timestamp']}}, // 必填，生成签名的时间戳
-            nonceStr: "{{$jsconfig['noncestr']}}", // 必填，生成签名的随机串
-            signature: "{{$jsconfig['sign']}}",// 必填，签名
-            jsApiList: ['chooseImage','uploadImage','getLocalImgData','startRecord'] // 必填，需要使用的JS接口列表
-        });
+        $(function () {
+            //克隆二级菜单
+            $(document).on('click','.two_btn',function () {
+                var _this = $(this);
+                //二级
+                var _div_two = _this.parents('.two_menu');
+                //二级个数
+                var two_length = _this.parents('.top_menu').find('.two_menu').length;
+                console.log(two_length);
+                if(two_length<3){
+                    _div_two.after(_div_two.clone());
+                }
 
-        wx.ready(function(){
+            })
 
+            //克隆一级菜单
+            $(document).on('click','.top_btn',function () {
+                var _this = $(this);
+                var _div = _this.parents('.top_menu');
+                //console.log(_div.length);
+                if($('.top_menu').length<3){
+                    _div.after(_div.clone());
+                }
+            });
+                //处理数组
+                $('#sub_btn').click(function(){
+                    top_info = {};
+                    $('.top_menu').each(function (i) {
+                        info = {};
+                        info['name']  = $(this).find('p').find('.top_name').val();
+                        data = {};
+                        $(this).find('.two_menu').each(function (e) {
+                            two_data = {};
+                            //console.log($(this).find('div').find('.two_btn_type').val());
+                            two_data['type'] = $(this).find('div').find('.two_btn_type').val();
+                            two_data['name'] = $(this).find('.two_btn_name').val();
 
-            $("#btn1").click(function () {
-                wx.chooseImage({
-                    count: 9, // 默认9
-                    sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-                    sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-                    success: function (res) {
-                        var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
-                    }
+                            if($(this).find('div').find('.two_btn_type').val() == 'view'){
+                                two_data['url'] = $(this).find('.two_btn_url').val();
+                            }else{
+                                two_data['key'] = $(this).find('.two_btn_key').val();
+                            }
+
+                            data[e] = two_data;
+                        });
+                        info['sub_button'] = data;
+                        top_info[i] = info;
+                    })
+                    $.ajax({
+                        headers: {
+                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                        },
+                        url:     '/weixin/createmenuaction',
+                        type:    'post',
+                        data:    top_info,
+                        dataType: 'json',
+                        success:   function (a) {
+                            if(a.errcode == 0){
+                                alert('发布成功')
+                            }
+                        }
+                    });
                 });
-            });
-            $("#btn3").click(function () {
-                wx.chooseImage({
-                    count: 9, // 默认9
-                    sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-                    sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-                    success: function (res) {
-                        var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
-                    }
-                });
-            });
-            $("#btn4").click(function () {
-                wx.chooseImage({
-                    count: 9, // 默认9
-                    sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-                    sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-                    success: function (res) {
-                        var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
-                    }
-                });
-            });
-            $("#btn5").click(function () {
-                wx.chooseImage({
-                    count: 9, // 默认9
-                    sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-                    sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-                    success: function (res) {
-                        var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
-                    }
-                });
-            });
-        });
+            })
     </script>
 @endsection
-
-
 
