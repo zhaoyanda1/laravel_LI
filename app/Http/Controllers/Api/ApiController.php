d928d100322f83097df26f6eb1636cb2aaff6fdb
zyd <zhaoyanda@163.com>
2019-03-22 15:55:27 +0800

    kd.

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -4,8 +4,9 @@ namespace App\Http\Controllers\Api;
 
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Request;
+
 
 class ApiController extends Controller
 {
